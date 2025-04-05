01e3797db2d067f2afc4887eb7ed2905cf6747f6
Shurderfer <aaron@kurlykris.com>
2025-04-04 20:14:24 -0500

    Intake should be fully implamented with filter
    TODO: TEST AND TUNE PROGRAM

--- Thing One/include/intake.h
+++ Thing One/include/intake.h
@@ -12,6 +12,14 @@ class Intake {
 
     colorSensor *color_sensor;
 
+    #define INTAKE_CLEAR 0
+    #define INTAKE_SPITTING 1
+
+    int intake_state = INTAKE_CLEAR;
+    int intake_state_machine_counter = 0;
+
+    #define SPIT_TIMER 20
+
 public:
 
     void initialize_intake(pros::MotorGroup lower_stage, pros::MotorGroup upper_stage, colorSensor color_sensor);
--- Thing One/src/intake.cpp
+++ Thing One/src/intake.cpp
@@ -12,9 +12,37 @@ void intake::initialize_intake(pros::MotorGroup lower_intake_stage, pros::MotorG
     upper_stage = &upper_intake_stage;
 }
 
+/*
+* Intakes with first stage like normal, but upper stage will try to spit out rings of the wrong color based on the color sensor.
+* Call it just like you would the normal intake function to use.
+*/
 void intake::filtered_intake() {
 
-    intake();
+    // Run first stage like normal
+    move_lower(intake_vel[0]);
+    
+    switch (intake_state) {
+        case INTAKE_CLEAR:
+            move_upper(intake_vel[0]);
+
+            // Change state if the wrong color is detected
+            if (colorSensor.sees_opps()) {
+                intake_state = INTAKE_SPITTING;
+            }
+            break;
+        case INTAKE_SPITTING:
+            move_upper(SPITTING_VEL);
+
+            intake_state_machine_timer++;
+
+            // Switch back when the timer runs out
+            if(intake_state_machine_timer >= SPIT_TIME) {
+                intake_state = INTAKE_CLEAR;
+                intake_state_machine_timer = 0;
+            }
+            break;
+    }
+    
 }
 
 void intake::intake() {
