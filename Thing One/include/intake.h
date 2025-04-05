db96ba7c516128a29fd6a52c298e07c729b2b67e
Shurderfer <aaron@kurlykris.com>
2025-04-04 19:21:20 -0500

    Encapsolating intake to add a color sensor

--- Thing One/include/intake.h
+++ Thing One/include/intake.h
@@ -1,12 +1,20 @@
 #pragma once
+#include 
 
 class Intake {
 
+    // 1 is first stage, 2 is second stage
     int intake_vel[2];
     int outtake_vel[2];
 
+    pros::MotorGroup *upper_stage;
+    pros::MotorGroup *lower_stage;
+
+    colorSensor *color_sensor;
+
 public:
 
+    void initialize_intake(pros::MotorGroup lower_stage, pros::MotorGroup upper_stage, colorSensor color_sensor);
     void initialize_intake(pros::MotorGroup lower_stage, pros::MotorGroup upper_stage);
 
     void filtered_intake();
--- Thing One/src/intake.cpp
+++ Thing One/src/intake.cpp
@@ -1,13 +1,34 @@
 #include "intake.h"
 
-void initialize_intake(pros::MotorGroup lower_stage, pros::MotorGroup upper_stage) {
-    
+void intake::initialize_intake(pros::MotorGroup lower_intake_stage, pros::MotorGroup upper_intake_stage, colorSensor colorSensor) {
+    lower_stage = &lower_intage_stage;
+    upper_stage = &upper_intake_stage;
+
+    color_sensor = &colorSensor;
 }
 
-void filtered_intake();
+void intake::initialize_intake(pros::MotorGroup lower_intake_stage, pros::MotorGroup upper_intake_stage) {
+    lower_stage = &lower_intage_stage;
+    upper_stage = &upper_intake_stage;
+}
 
-void intake();
-void outtake();
+void intake::filtered_intake() {
 
-void move_upper(int voltage);
-void move_lower(int voltage);
+    intake();
+}
+
+void intake::intake() {
+    upper_stage.move(intake_vel[0]);
+    lower_stage.move(intake_vel[1]);
+}
+void intake::outtake() {
+    upper_stage.move(outtake_vel[0]);
+    lower_stage.move(outtake_vel[1]);
+}
+
+void intake::move_upper(int voltage) {
+    lower_stage.move(voltage);
+}
+void intake::move_lower(int voltage) {
+    lower_stage.move(voltage);
+}
