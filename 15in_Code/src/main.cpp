82f4cb3f2597bf0c544b40c43e51628b5d5006cc
Daniel Kohler <danielkohler737@gmail.com>
2025-04-21 15:06:14 -0500

    New fresh template
    Set up clean templates for both robots with RoboDash autonomous selectors

---
+++ 15in_Code/src/main.cpp
@@ -0,0 +1,146 @@
+#include "main.h"
+#include "lemlib/api.hpp"
+#include "robodash/api.h"
+
+rd::Selector selector({
+    {"SKILLS", skills},
+    {"BLUE SIDE GOAL", blueSideGoal},
+    {"BLUE MID GOAL", blueMidGoal},
+    {"RED SIDE GOAL", redSideGoal},
+    {"RED MID GOAL", redMidGoal},
+});
+rd::Console console;
+
+
+pros::Controller controller(pros::E_CONTROLLER_MASTER);
+
+pros::adi::Encoder horizontal_encoder('A', 'B');
+pros::adi::Encoder vertical_encoder('C', 'D');
+
+lemlib::TrackingWheel horizontal_tracking_wheel(&horizontal_encoder, .11, 0);
+lemlib::TrackingWheel vertical_tracking_wheel(&vertical_encoder, .11, 0);
+
+pros::adi::Pneumatics clamp('e', false);
+pros::adi::Pneumatics rushMech('f', false);
+
+// left motor group
+pros::MotorGroup left_motor_group({1, -20, -16}, pros::MotorGears::blue);
+// right motor group
+pros::MotorGroup right_motor_group({-8, 11, 12}, pros::MotorGears::blue);
+
+pros::MotorGroup intake({-9,10}, pros::MotorGears::blue);
+
+
+// drivetrain settings
+lemlib::Drivetrain drivetrain(&left_motor_group, // left motor group
+                              &right_motor_group, // right motor group
+                              11.3, // 11.3 inch track width
+                              lemlib::Omniwheel::NEW_275, // using new 4" omnis
+                              600, // drivetrain rpm is 360
+                              2 // horizontal drift is 2 (for now)
+);
+
+// imu
+pros::Imu imu(13);
+
+
+// odometry settings
+lemlib::OdomSensors sensors(&vertical_tracking_wheel, // vertical tracking wheel 1, set to null
+                            nullptr, // vertical tracking wheel 2, set to nullptr as we are using IMEs
+                            &horizontal_tracking_wheel, // horizontal tracking wheel 1
+                            nullptr, // horizontal tracking wheel 2, set to nullptr as we don't have a second one
+                            &imu // inertial sensor
+);
+
+// lateral PID controller
+lemlib::ControllerSettings lateral_controller(5.5, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              1, // derivative gain (kD)
+                                              3, // anti windup
+                                              1, // small error range, in inches
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in inches
+                                              500, // large error range timeout, in milliseconds
+                                              20 // maximum acceleration (slew)
+);
+
+// angular PID controller
+lemlib::ControllerSettings angular_controller(1, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              2, // derivative gain (kD)
+                                              3, // anti windup
+                                              1, // small error range, in degrees
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in degrees
+                                              500, // large error range timeout, in milliseconds
+                                              0 // maximum acceleration (slew)
+);
+
+// create the chassis
+lemlib::Chassis chassis(drivetrain, // drivetrain settings
+                        lateral_controller, // lateral PID settings
+                        angular_controller, // angular PID settings
+                        sensors // odometry sensors
+);
+
+
+/**
+ * Runs initialization code. This occurs as soon as the program is started.
+ *
+ * All other competition modes are blocked by initialize; it is recommended
+ * to keep execution time for this mode under a few seconds.
+ */
+void initialize() {}
+
+/**
+ * Runs while the robot is in the disabled state of Field Management System or
+ * the VEX Competition Switch, following either autonomous or opcontrol. When
+ * the robot is enabled, this task will exit.
+ */
+void disabled() {}
+
+/**
+ * Runs after initialize(), and before autonomous when connected to the Field
+ * Management System or the VEX Competition Switch. This is intended for
+ * competition-specific initialization routines, such as an autonomous selector
+ * on the LCD.
+ *
+ * This task will exit when the robot is enabled and autonomous or opcontrol
+ * starts.
+ */
+void competition_initialize() {}
+
+/**
+ * Runs the user autonomous code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the autonomous
+ * mode. Alternatively, this function may be called in initialize or opcontrol
+ * for non-competition testing purposes.
+ *
+ * If the robot is disabled or communications is lost, the autonomous task
+ * will be stopped. Re-enabling the robot will restart the task, not re-start it
+ * from where it left off.
+ */
+void autonomous() {
+    selector.run_auton();
+}
+
+/**
+ * Runs the operator control code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the operator
+ * control mode.
+ *
+ * If no competition control is connected, this function will run immediately
+ * following initialize().
+ *
+ * If the robot is disabled or communications is lost, the
+ * operator control task will be stopped. Re-enabling the robot will restart the
+ * task, not resume it from where it left off.
+ */
+
+void opcontrol() {
+    console.println("Hello");
+    selector.run_auton();
+
+}
